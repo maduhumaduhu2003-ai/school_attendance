278b727ad2f7ff0ab236d9235f8608b8aa353add
maduhumaduhu2003-ai <maduhumaduhu2003@gmail.com>
2026-01-20 00:01:31 +0300

    Add Render domain to ALLOWED_HOSTS

--- school_attendance/settings.py
+++ school_attendance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qq2s%x+g*5lua8p4q-vzgmmlxpg5icm-1#rnumnmdshb-^u_7p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['school-attendance-1.onrender.com']
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
