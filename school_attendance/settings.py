b75df7ffb8011d75730fbd683a64cff1fb8e2b08
maduhumaduhu2003-ai <maduhumaduhu2003@gmail.com>
2026-01-20 00:47:55 +0300

    Switch DB to PostgreSQL and update settings

--- school_attendance/settings.py
+++ school_attendance/settings.py
@@ -90,12 +90,19 @@ WSGI_APPLICATION = 'school_attendance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'school_attendance_db'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
