07ee01acab8fb2a98315ab4d622bc4687ebd460e
maduhumaduhu2003-ai <maduhumaduhu2003@gmail.com>
2026-01-23 01:52:33 +0300

    Update project

--- school_attendance/settings.py
+++ school_attendance/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qq2s%x+g*5lua8p4q-vzgmmlxpg5icm-1#rnumnmdshb-^u_7p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['school-attendance-1.onrender.com', "localhost","127.0.0.1",]
 
