f5d57ea1f57812bfa19d1bea531678cf8f129e3b
maduhumaduhu2003-ai <maduhumaduhu2003@gmail.com>
2026-02-05 04:19:55 +0300

    Update: Added SMS functionality

--- school_attendance/settings.py
+++ school_attendance/settings.py
@@ -105,5 +105,17 @@ TEMPLATES = [
     },
 ]
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Automatically configure Cloudinary using CLOUDINARY_URL
+cloudinary.config(
+    cloud_name=config('CLOUDINARY_URL').split(':')[1].split('@')[1],
+    api_key=config('CLOUDINARY_URL').split(':')[1].split('@')[0],
+    api_secret=config('CLOUDINARY_URL').split(':')[2]
+)
+
+
 ROOT_URLCONF = 'school_attendance.urls'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
