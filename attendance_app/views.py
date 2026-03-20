6134e7c113670cc0c98e08c7ea9f6521ae21ebbd
maduhumaduhu2003-ai <maduhumaduhu2003@gmail.com>
2026-03-20 11:21:47 +0300

    Add changes

--- attendance_app/views.py
+++ attendance_app/views.py
@@ -109,7 +109,7 @@ User = get_user_model()
 #school GPS coordinates
 SCHOOL_LAT = -6.92673   
 SCHOOL_LNG = 37.56749   
-MAX_DISTANCE_METERS = 200  
+MAX_DISTANCE_METERS = 100  
 
 def distance_in_meters(lat1, lon1, lat2, lon2):
     """Haversine formula to calculate distance in meters between two GPS points"""
